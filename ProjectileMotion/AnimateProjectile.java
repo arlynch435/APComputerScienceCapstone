e5404bbc5f49c13cfda511f731ad262ebb03eaa3
arlynch435 <ARLynch@student.naperville203.org>
2016-04-27 15:03:15 -0500

    started timerTask and timer things :/

---
+++ ProjectileMotion/AnimateProjectile.java
@@ -0,0 +1,38 @@
+
+import java.util.TimerTask;
+/**
+ * Write a description of class AnimateProjectile here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class AnimateProjectile extends TimerTask
+{
+    /** description of instance variable x (add comment for each instance variable) */
+
+    /**
+     * Default constructor for objects of class AnimateProjectile
+     */
+    public AnimateProjectile()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *    that describes the operation of the method
+     *
+     * @pre        preconditions for the method
+     *            (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *            (what the method guarantees upon completion)
+     * @param    y    description of parameter y
+     * @return    description of the return value
+     */
+    public void run()
+    {
+        // put your code here animate();
+    }
+
+}
